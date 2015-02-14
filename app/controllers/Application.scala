3721e4bcb9e17596d56f4f334513499849097860
ajchau <albertjmchau@gmail.com>
2015-02-14 14:42:25 -0800

    testing out redeploy

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,25 @@
 package controllers
 
 import play.api._
+import play.api.Play.current
 import play.api.mvc._
+import play.api.db._
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+//    val conn = DB.getConnection()
+//    var outString = "Number is "
+//    try {
+//      val stmt = conn.createStatement
+//      val rs = stmt.executeQuery("SELECT * from users limit 10")
+//      while (rs.next()) {
+//        outString += rs.getString(1)
+//      }
+//    } finally {
+//      conn.close()
+//    }
+    Ok(views.html.index("Your new application is ready." + "outString"))
   }
 
 }
